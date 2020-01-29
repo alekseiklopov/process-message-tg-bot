fafee9c15d6b3685f4c5a8305e6b1a70599a6334
Ubuntu <ubuntu@ubuntu>
2020-01-29 16:31:30 +0000

    Remove trash

--- src/testing.py
+++
@@ -1,9 +0,0 @@
-from processing import *
-
-
-if __name__ == "__main__":
-    image = cv2.imread("../data/images/image2.jpg")
-    print(image.shape)
-    print(count_faces(image))
-    print(image)
-    print(type(image))
