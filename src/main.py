1198e73849f9ed483c51ef552390d78d2d984bdd
Ubuntu <ubuntu@ubuntu>
2020-01-29 10:04:40 +0000

    Add stubs in answers for photo/audio

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import os
 import telebot
+from processing import *
+
 
 TG_TOKEN = os.environ.get("TG_TOKEN")
 bot = telebot.TeleBot(TG_TOKEN)
-my_keyboard = telebot.types.ReplyKeyboardMarkup()
+my_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
 my_keyboard.row("Информация")
 
 
@@ -19,10 +21,33 @@ def send_text(message):
 1. Сохранять аудиосообщения из диалогов в базу данных (СУБД или на диск) по идентификаторам пользователей.
 2. Конвертировать все аудиосообщения в формат wav с частотой дискретизации 16kHz. Формат записи: uid —> [audio_message_0, audio_message_1, ..., audio_message_N].
 3. Определять, есть ли лицо на отправляемых фотографиях или нет, и сохранять только те, где оно есть.
+
 Кто мой создатель?
 Алексей Клопов - @itsmealeksei - klopov.pw""")
     elif message.text.lower() == "привет":
         bot.send_message(message.chat.id, "Привет!")
+
+@bot.message_handler(content_types=["document"])
+def handle_docs(message):
+    bot.reply_to(message, "Отправьте фото/аудиосообщение, не файл!")
+
+
+@bot.message_handler(content_types=["photo"])
+def handle_photo(message):
+    file_ID = message.photo[-1].file_id 
+    file_info = bot.get_file(file_ID)
+    downloaded_file = bot.download_file(file_info.file_path)
+    # answer = process_photo(downloaded_file)
+    answer = "ok"
+    # print(count_faces(cv2.imread("../data/images/image2.jpg")))
+    bot.reply_to(message, answer)
+
+
+@bot.message_handler(content_types=["voice"])
+def handle_audio(message):
+    bot.reply_to(message, message)
+
+
 
 
 bot.polling()
